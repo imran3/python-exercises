e116e052220c7680d174a37db78345ab37e16bcc
Imran Azam <imran.azam@and.digital>
2023-06-08 17:13:08 +0100

    UP-1: set param types

--- fundamentals/src/sort_dictionary.py
+++ fundamentals/src/sort_dictionary.py
@@ -1,8 +1,8 @@
 # given a dictionary of contacts, return list of names sorted alphabetically
-def sort_contacts_list(dictionary):
+def sort_contacts_list(dictionary: dict):
     sorted_list =  sorted(dictionary.items(), key = lambda x: x[1])
-    names = list(x[1] for x in sorted_list)
-    return names
+    sorted_names_list = list(x[1] for x in sorted_list)
+    return sorted_names_list
 
-def sort_contacts_list_v2(dictionary):
+def sort_contacts_list_v2(dictionary: dict):
     return sorted(dictionary.values())
