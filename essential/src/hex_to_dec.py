d65c1397c5423f20437fa4fe51bdfa6f6e64f3cd
imran3 <imranazam3@gmail.com>
2023-06-12 16:19:34 +0100

    UP-2: solution and test for hex to dec challenge

---
+++ essential/src/hex_to_dec.py
@@ -0,0 +1,18 @@
+hexNumbers = {
+    '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+    'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15
+}
+
+# Converts a string hexadecimal number into an integer decimal
+# If hexNum is not a valid hexadecimal number, returns None
+def hex_to_dec(hexNum):
+    if not isinstance(hexNum, str): return None
+    
+    charLocation = 0
+    res = 0
+    for char in reversed(hexNum):
+        if char not in hexNumbers: return None
+        
+        res += hexNumbers[char] * (16 ** charLocation)
+        charLocation +=1
+    return res
---
+++ essential/tests/hextodec_test.py
@@ -0,0 +1,14 @@
+import pytest
+from src.hex_to_dec import hex_to_dec
+
+@pytest.mark.parametrize('hex_num, expected_dec', [('', 0),('1A', 26), ('123', 291),('E1F2BA', 14807738), ('ABC', 2748), ('124BBCDFE', 4911255038)])
+def test_valid_hexadecimal(hex_num, expected_dec):
+    assert hex_to_dec(hex_num) == expected_dec
+
+@pytest.mark.parametrize('hex_num', ['G', 'XYZ', '$!%'])
+def test_invalid_hexadecimal(hex_num):
+    assert hex_to_dec(hex_num) is None
+
+@pytest.mark.parametrize('hex_num', [123, ['A', 'B', 'C']])
+def test_non_string_input(hex_num):
+    assert hex_to_dec(hex_num) is None
