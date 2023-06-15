fb262c99773ead87ba76b16bc42f79a2db0e3585
imran3 <imranazam3@gmail.com>
2023-06-15 16:41:18 +0100

    UP-2: merged fundamentals and essentials

--- tests/transform_string_test.py
+++ tests/transform_string_test.py
@@ -1,17 +1,20 @@
 import pytest
 from src.transform_string import hacker_speak
 
+
 def test_hacker_speak_replace():
     input = "this is so cool!"
     expected = "th15 15 50 c00l!"
     res = hacker_speak(input)
     assert res == expected
 
+
 def test_hacker_speak_replace_all():
     input = "aaaeeeiiiioooss ssiii eeeooo sssaa aeios"
     expected = "444333111100055 55111 333000 55544 43105"
     res = hacker_speak(input)
     assert res == expected
+
 
 def test_hacker_speak_no_replace():
     input = "Fly thy crypt!"
